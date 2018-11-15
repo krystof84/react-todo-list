f3eeb365b8e91e9c8df35726667488cac31ad07c
Krzysztof <krisczajka@gmail.com>
2018-11-15 11:28:17 +0100

    Basic app structure

--- src/App.js
+++ src/App.js
@@ -3,25 +3,36 @@ import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+
+          <div className="header">
+            <div className="row">
+              <div className="col-12 col-md-6">
+                <div className="form-group">
+                  <input type="text" className="form-control"/>
+                  <button type="submit" className="btn btn-primary">Add task</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12 col-md-6">
+
+              Todo tasks list
+              
+            </div>
+            <div className="col-12 col-md-6">
+              Done tasks list
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
