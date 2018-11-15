8b4d0ee0c1f82358c797760bee6848404b1309f9
Krzysztof <krisczajka@gmail.com>
2018-11-15 10:41:32 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +10,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
