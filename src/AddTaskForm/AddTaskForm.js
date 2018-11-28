f42c5a2eabc23caa4e4d422c2a720e0343dbb696
Krzysztof <krisczajka@gmail.com>
2018-11-28 19:26:36 +0100

    Focus on input when component did mount

--- src/AddTaskForm/AddTaskForm.js
+++ src/AddTaskForm/AddTaskForm.js
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const AddTaskForm = (props) => {
-    return (
-        <div className="AddTask">
-            <div className="row">
-                <div className="col-12 col-md-6">
-                    <div className="form-group">
-                        <input 
-                            type="text" 
-                            className="form-control AddTask__input"
-                            onChange={props.updateInput}
-                            onKeyPress={event => {
-                                if(event.key === 'Enter') {
-                                    props.newTask();
-                                }
-                            }}
-                            value={props.inputVal}/>
-                        <button 
-                            type="submit" 
-                            className="btn btn-primary AddTask__button"
-                            onClick={props.newTask}>Add task</button>
+class AddTaskForm extends Component {
+
+    focusTextInput() {
+        // Explicitly focus the text input using the raw DOM API
+        // Note: we're accessing "current" to get the DOM node
+        this.textInput.current.focus();
+      }
+
+    componentDidMount() {
+        document.getElementById('textInput').focus();
+    }
+
+    render() {
+        return (
+            <div className="AddTask">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="form-control AddTask__input"
+                                onChange={this.props.updateInput}
+                                onKeyPress={event => {
+                                    if(event.key === 'Enter') {
+                                        this.props.newTask();
+                                    }
+                                }}
+                                value={this.props.inputVal}
+                                id="textInput"/>
+                            <button 
+                                type="submit" 
+                                className="btn btn-primary AddTask__button"
+                                onClick={this.props.newTask}>Add task</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default AddTaskForm;
